87d76bff7a61beb023205e4470045e328c76af87
Jiyang Li <jiyang725@gmail.com>
2018-11-25 12:11:30 -0600

    create struct

--- cse489589_assignment3/jiyangli/include/routing_alg.h
+++ cse489589_assignment3/jiyangli/include/routing_alg.h
@@ -2,10 +2,26 @@
 
 void router_init(char* init_payload);
 
-struct __attribute__((__packed__)) ROUTING_TABLE
-    {
-        uint16_t router_id;
-        uint16_t padding;
-        uint16_t next_hop_id;
-        uint16_t router_cost;
-    };
+#define PACKET_USING_STRUCT // Comment this out to use alternate packet crafting technique
+
+#ifdef PACKET_USING_STRUCT
+
+	struct __attribute__((__packed__)) ROUTING_TABLE
+	{
+	    uint16_t router_id;
+	    uint16_t padding;
+	    uint16_t next_hop_id
+	    uint16_t router_cost;
+	};
+
+	struct __attribute__((__packed__)) DATA_PACKET_HEADER
+	{
+		uint32_t dest_ip_addr;
+	    uint8_t transfer_id;
+	    uint8_t ttl;
+	    uint16_t seq;
+	    uint8_t fin;
+	    uint8_t padding;
+	};
+
+#endif
